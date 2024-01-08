ba37eca2966fff97bf6cc88fd56e0c638265f09c
Selim Lakehal <selim.lakehal@polymtl.ca>
2024-01-08 09:39:55 -0500

    Add response in dashboard page

--- site/src/pages/DashboardPage.jsx
+++ site/src/pages/DashboardPage.jsx
@@ -1,8 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { serverURL } from "../consts/consts";
 
 export default function DashboardPage() {
   //TODO : Add conditionnal rendering.
   //TODO : Create logout button that makes the appropriate fetch req.
   const [isSignedIn, setSignIn] = useState(false);
-  return <p>No current users</p>;
+  const [user, setUser] = useState({});
+  useEffect(async () => {
+    const response = await fetch(`${serverURL}/auth/profile`, {
+      withCredentials: true,
+      credentials: "include",
+    });
+    const data = await response.json();
+    console.log(data);
+  }, []);
+  return <>{isSignedIn ? <p>{user.username}</p> : <p>No current users</p>}</>;
 }
